124fbb9e0b556d666a9b2d519ec2611bca522db0
Trevor Gray <trevormgray@gmail.com>
2025-01-15 14:24:16 -0500

    Added multiple grades test

--- test_grades.py
+++ test_grades.py
@@ -10,3 +10,7 @@ def test_zero_grades():
 def test_single_grade():
     grades = [42]
     assert compute_hw_average(grades) == 42
+
+def test_multiple_grades():
+    grades = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert compute_hw_average(grades) = 5.5
